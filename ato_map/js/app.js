6e4dd8f7dab1647a0ee3dc0d2a13028e80bed993
Mykola Prymak <mykola.prymak@gmail.com>
2014-08-14 17:02:22 +0300

    ato_map: Style fixes. Show help. Add storage engine.

--- ato_map/js/app.js
+++ ato_map/js/app.js
@@ -1,6 +1,6 @@
 "use strict";
 
-define(['jquery', 'map_data'], function($, map_data) {
+define(['jquery', 'map_data', 'storage'], function($, map_data, Storage) {
   var KEY_ARROW_LEFT = 37;
   var KEY_ARROW_RIGHT = 39;
   var $map;
@@ -11,10 +11,10 @@ define(['jquery', 'map_data'], function($, map_data) {
       var $container = $('#content');
       var _this = this;
 
+      var storage = new Storage('ato', {hideHelp: false});
+
       $map = $('#map');
       $mapDate = $('#map-date');
-
-      
 
       $(document).on('keydown', function(evt) {
         var keyCode = evt.keyCode;
@@ -31,7 +31,15 @@ define(['jquery', 'map_data'], function($, map_data) {
       $('#next').click(function() {
         _this.showNext();
       });
-      
+
+      if (storage.get('hideHelp')) {
+        $('#helpAlert').css('display', 'none');
+      }
+      $('#helpAlert .close').click(function() {
+        storage.set('hideHelp', true);
+        $('#helpAlert').css('display', 'none');
+      });
+
       this.mapCurrentIdx= map_data.length - 1;
       this.mapMaxIdx= map_data.length - 1;
       this.showMap(map_data[this.mapCurrentIdx]);
